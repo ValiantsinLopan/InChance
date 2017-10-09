a31702dba04cd881c1e3c87b091bbd8d184751e3
Valiantsin <valiantsin_lopan@live.com>
2017-10-09 21:59:29 +0300

    Loading comments by clicking extension button

---
+++ content_script.js
@@ -0,0 +1,5 @@
+function clickLoadMoreComments() {
+    var loadMoreCommentsButton = document.getElementsByClassName("_m3m1c _1s3cd")[0];
+    loadMoreCommentsButton.click();
+}
+clickLoadMoreComments();
