7e391786aa45bcdb0a61711999a206149bd0b49a
Valiantsin <valiantsin_lopan@live.com>
2017-10-10 23:07:10 +0300

    Loading all comments added

--- content_script.js
+++ content_script.js
@@ -1,5 +1,10 @@
 function clickLoadMoreComments() {
     var loadMoreCommentsButton = document.getElementsByClassName("_m3m1c _1s3cd")[0];
-    loadMoreCommentsButton.click();
+    var timerId = setTimeout(function clickAgain() {
+        if (document.body.contains(loadMoreCommentsButton)){
+        loadMoreCommentsButton.click();
+        }
+        timerId = setTimeout(clickAgain, 1000);
+      }, 1000);
 }
 clickLoadMoreComments();
