1e90883f94699707b838c1230a4b5c749e36ae21
Valiantsin <valiantsin_lopan@live.com>
2017-10-17 22:27:22 +0300

    jQuery added

--- content_script.js
+++ content_script.js
@@ -7,8 +7,21 @@ function clickLoadMoreComments() {
         timerId = setTimeout(clickAgain, 1000);
         if (!document.body.contains(loadMoreCommentsButton)){
             clearTimeout(timerId);
-            alert(document.getElementsByClassName("_ezgzd").length - 1);
+            //alert(document.getElementsByClassName("_ezgzd").length - 1);
         }
       });
+}
+
+function randomInteger(min, max) {
+    var rand = min - 0.5 + Math.random() * (max - min + 1)
+    rand = Math.round(rand);
+    return rand;
+}
+
+function getRandomComment(){
+    var comments = document.getElementsByClassName("_ezgzd");
+    var commentsCount = comments.length - 1;
+    var randomCommentIndex = randomInteger(1, commentsCount);
+    return comments[randomCommentIndex];
 }
 clickLoadMoreComments();
--- popup.js
+++ popup.js
@@ -1,7 +1,12 @@
 function injectTheScript() {
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         chrome.tabs.executeScript(tabs[0].id, {file: "content_script.js"});
+        
     });
 }
 
 document.getElementById('loadcomments').addEventListener('click', injectTheScript);
+
+//$(document).ready(function(){
+//    $('.btn').text('Текст, для изменения');
+//})
