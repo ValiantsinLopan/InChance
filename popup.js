53ef0a92a0b1f9a63fdb9e12d13d75b8b648965c
Valiantsin <valiantsin_lopan@live.com>
2017-11-17 00:54:12 +0300

    Add random comment by click on button

--- popup.js
+++ popup.js
@@ -1,3 +1,11 @@
+var innerHTMLs = new Array();
+
+function randomInteger(min, max) {
+    var rand = min - 0.5 + Math.random() * (max - min + 1)
+    rand = Math.round(rand);
+    return rand;
+}
+
 function injectTheScript() {
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         chrome.tabs.executeScript(tabs[0].id, {file: "content_script.js"});
@@ -9,10 +17,14 @@ chrome.runtime.onConnect.addListener(function(port) {
         document.getElementById("commentsCounter").innerHTML = msg.commentsCount;
         if (msg.message == "finished"){
             document.getElementById("loadcomments").innerHTML = "Comments uploaded";
-            console.log(msg.comments);
+            innerHTMLs = JSON.parse(msg.comments);
         }
 
     });
   });
 
-document.getElementById('loadcomments').addEventListener('click', injectTheScript);
+$('#loadcomments').on('click', injectTheScript);
+$('#getSingleWinner').click( function(){
+    document.getElementById("singleWinner").innerHTML = innerHTMLs[randomInteger(1, innerHTMLs.length)];
+    console.log(document.getElementsByClassName('_2g7d5').href);
+});
