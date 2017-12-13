0800779eaa0e38c08ad4d32030304bb8c596c9ec
Valiantsin <valiantsin_lopan@live.com>
2017-12-13 21:24:28 +0300

    Modified getRandom() : if winners count more then contender method return all contenders

--- popup.js
+++ popup.js
@@ -10,8 +10,9 @@ function getRandom(arr, n) {
     var result = new Array(n),
         len = arr.length,
         taken = new Array(len);
-    if (n > len)
-        throw new RangeError("getRandom: more elements taken than available");
+    if (n > len){
+        n=len;
+    }
     while (n--) {
         var x = Math.floor(Math.random() * len);
         result[n] = arr[x in taken ? taken[x] : x];
